98777b1ab815095bf693087bf8c64901b44af3ad
Weiwei Shi <weiwei@yahoo-inc.com>
2014-04-06 11:39:52 +0800

    first problem

---
+++ src/leecode/TwoSum.java
@@ -0,0 +1,13 @@
+package leecode;
+
+public class TwoSum {
+
+	public int[] twoSum(int[] numbers, int target) {
+		return null;
+	}
+
+	public static void main(String[] args) {
+		TwoSum c = new TwoSum();
+		System.out.println(c.twoSum(new int[] { 2, 7, 11, 15 }, 9));
+	}
+}
