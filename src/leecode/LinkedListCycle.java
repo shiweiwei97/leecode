76ae3aaccb6450ee014d875793554a56cb8c14b4
Weiwei Shi <weiwei@yahoo-inc.com>
2014-09-21 14:21:43 +0800

    linked list cycle

--- src/leecode/LinkedListCycle.java
+++ src/leecode/LinkedListCycle.java
@@ -9,16 +9,31 @@ import leecode.common.ListNode;
  * 
  */
 public class LinkedListCycle {
-    
-    
-    public boolean hasCycle(ListNode head) {
-        return false;
 
+    public boolean hasCycle(ListNode head) {
+
+        ListNode p = new ListNode(0);
+        p.next = head;
+
+        ListNode q = new ListNode(0);
+        q.next = head;
+
+        while (p != null && q != null && p != q) {
+            p = p.next;
+            q = q.next;
+            if (q != null) {
+                q = q.next;
+            }
+
+            if (p == q && p != null && q != null) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
 
     }
-
 }
