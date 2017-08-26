417deea2ef8a9cbc256b85017a42a109f38d4b73
Weiwei Shi <shiweiwei97@gmail.com>
2017-08-26 17:00:17 +0800

    tab -> space

--- src/leecode/FourSum.java
+++ src/leecode/FourSum.java
@@ -13,81 +13,81 @@ import java.util.List;
  */
 public class FourSum {
 
-	public List<List<Integer>> fourSum(int[] nums, int target) {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
 
-		Arrays.sort(nums);
+        Arrays.sort(nums);
 
-		List<List<Integer>> result = new ArrayList<List<Integer>>();
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
 
-		for (int i = 0; i < nums.length - 3; i++) {
-			// skip too large
-			if (sumByRange(nums, i, 4) > target)
-				continue;
+        for (int i = 0; i < nums.length - 3; i++) {
+            // skip too large
+            if (sumByRange(nums, i, 4) > target)
+                continue;
 
-			// skip too small
-			if (nums[i] + sumByRange(nums, nums.length - 3, 3) < target)
-				continue;
+            // skip too small
+            if (nums[i] + sumByRange(nums, nums.length - 3, 3) < target)
+                continue;
 
-			// skip duplicate
-			if (i > 0 && nums[i] == nums[i - 1])
-				continue;
+            // skip duplicate
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
 
-			for (int j = i + 1; j < nums.length - 2; j++) {
-				// skip too large
-				if (nums[i] + sumByRange(nums, j, 3) > target)
-					continue;
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                // skip too large
+                if (nums[i] + sumByRange(nums, j, 3) > target)
+                    continue;
 
-				// skip too small
-				if (nums[i] + nums[j] + sumByRange(nums, nums.length - 2, 2) < target)
-					continue;
+                // skip too small
+                if (nums[i] + nums[j] + sumByRange(nums, nums.length - 2, 2) < target)
+                    continue;
 
-				// skip duplicate
-				if (j > i + 1 && nums[j] == nums[j - 1])
-					continue;
+                // skip duplicate
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
 
-				// start 2Sum search
-				int lo = j + 1, hi = nums.length - 1, sum = target - nums[i] - nums[j];
-				while (lo < hi) {
-					if (nums[lo] + nums[hi] == sum) {
-						// found a match
-						result.add(Arrays.asList(nums[i], nums[j], nums[lo], nums[hi]));
+                // start 2Sum search
+                int lo = j + 1, hi = nums.length - 1, sum = target - nums[i] - nums[j];
+                while (lo < hi) {
+                    if (nums[lo] + nums[hi] == sum) {
+                        // found a match
+                        result.add(Arrays.asList(nums[i], nums[j], nums[lo], nums[hi]));
 
-						// skip on duplicate nums[lo] and nums[hi]
-						while (lo < hi && nums[lo] == nums[lo + 1])
-							lo++;
-						while (lo < hi && nums[hi] == nums[hi - 1])
-							hi--;
-						lo++;
-						hi--;
-					} else if (nums[lo] + nums[hi] < sum) {
-						// increase nums[lo]
-						lo++;
-					} else {
-						// decrease nums[hi]
-						hi--;
-					}
-				}
-			}
-		}
+                        // skip on duplicate nums[lo] and nums[hi]
+                        while (lo < hi && nums[lo] == nums[lo + 1])
+                            lo++;
+                        while (lo < hi && nums[hi] == nums[hi - 1])
+                            hi--;
+                        lo++;
+                        hi--;
+                    } else if (nums[lo] + nums[hi] < sum) {
+                        // increase nums[lo]
+                        lo++;
+                    } else {
+                        // decrease nums[hi]
+                        hi--;
+                    }
+                }
+            }
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	private int sumByRange(int[] nums, int start, int len) {
-		int sum = 0;
-		for (int i = start; i < start + len && i < nums.length; i++) {
-			sum += nums[i];
-		}
-		return sum;
-	}
+    private int sumByRange(int[] nums, int start, int len) {
+        int sum = 0;
+        for (int i = start; i < start + len && i < nums.length; i++) {
+            sum += nums[i];
+        }
+        return sum;
+    }
 
-	public static void main(String[] args) {
-		FourSum c = new FourSum();
-		int[] nums;
-		int target;
+    public static void main(String[] args) {
+        FourSum c = new FourSum();
+        int[] nums;
+        int target;
 
-		nums = new int[] { 1, 0, -1, 0, -2, 2 };
-		target = 0;
-		System.out.println(c.fourSum(nums, target));
-	}
+        nums = new int[] { 1, 0, -1, 0, -2, 2 };
+        target = 0;
+        System.out.println(c.fourSum(nums, target));
+    }
 }
--- src/leecode/SubstringWithConcatenationAllWords.java
+++ src/leecode/SubstringWithConcatenationAllWords.java
@@ -13,77 +13,77 @@ import java.util.Map;
  */
 public class SubstringWithConcatenationAllWords {
 
-	public List<Integer> findSubstring(String s, String[] words) {
-		List<Integer> result = new ArrayList<Integer>();
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> result = new ArrayList<Integer>();
 
-		if (words.length < 1)
-			return result;
+        if (words.length < 1)
+            return result;
 
-		int len = words[0].length() * words.length;
-		if (s.length() < len)
-			return result;
+        int len = words[0].length() * words.length;
+        if (s.length() < len)
+            return result;
 
-		// pre-compute map of word => count
-		Map<String, Integer> map = new HashMap<String, Integer>();
-		for (String word : words) {
-			if (map.containsKey(word)) {
-				map.put(word, map.get(word) + 1);
-			} else {
-				map.put(word, 1);
-			}
-		}
+        // pre-compute map of word => count
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for (String word : words) {
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+            } else {
+                map.put(word, 1);
+            }
+        }
 
-		for (int i = 0; i <= s.length() - len; i++) {
-			Map<String, Integer> copyMap = new HashMap<String, Integer>(map);
-			if (match(s.substring(i, i + len), words, copyMap)) {
-				result.add(i);
-			}
-		}
+        for (int i = 0; i <= s.length() - len; i++) {
+            Map<String, Integer> copyMap = new HashMap<String, Integer>(map);
+            if (match(s.substring(i, i + len), words, copyMap)) {
+                result.add(i);
+            }
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	private boolean match(String s, String[] words, Map<String, Integer> map) {
+    private boolean match(String s, String[] words, Map<String, Integer> map) {
 
-		int step = words[0].length();
-		int len = step * words.length;
-		if (s.length() != len)
-			return false;
+        int step = words[0].length();
+        int len = step * words.length;
+        if (s.length() != len)
+            return false;
 
-		for (int i = 0; i < s.length(); i += step) {
-			String word = s.substring(i, i + step);
+        for (int i = 0; i < s.length(); i += step) {
+            String word = s.substring(i, i + step);
 
-			if (map.containsKey(word)) {
-				// reduce count or remove entry if a word is found
-				int newVal = map.get(word) - 1;
-				if (newVal > 0) {
-					map.put(word, newVal);
-				} else {
-					map.remove(word);
-				}
-			} else {
-				// not a match
-				break;
-			}
-		}
+            if (map.containsKey(word)) {
+                // reduce count or remove entry if a word is found
+                int newVal = map.get(word) - 1;
+                if (newVal > 0) {
+                    map.put(word, newVal);
+                } else {
+                    map.remove(word);
+                }
+            } else {
+                // not a match
+                break;
+            }
+        }
 
-		// map is empty if every word is used
-		return map.isEmpty();
-	}
+        // map is empty if every word is used
+        return map.isEmpty();
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SubstringWithConcatenationAllWords c = new SubstringWithConcatenationAllWords();
+        SubstringWithConcatenationAllWords c = new SubstringWithConcatenationAllWords();
 
-		String s;
-		String[] words;
+        String s;
+        String[] words;
 
-		s = "wordgoodgoodgoodbestword";
-		words = new String[] { "word", "good", "best", "good" };
-		System.out.println(c.findSubstring(s, words));
+        s = "wordgoodgoodgoodbestword";
+        words = new String[] { "word", "good", "best", "good" };
+        System.out.println(c.findSubstring(s, words));
 
-		s = "barfoothefoobarman";
-		words = new String[] { "foo", "bar" };
-		System.out.println(c.findSubstring(s, words));
-	}
+        s = "barfoothefoobarman";
+        words = new String[] { "foo", "bar" };
+        System.out.println(c.findSubstring(s, words));
+    }
 }
