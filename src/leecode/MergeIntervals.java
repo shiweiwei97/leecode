c649690fc93217af2a98c0dc10e10003fef449e6
Weiwei Shi <shiweiwei97@gmail.com>
2017-09-03 15:25:47 +0800

    Merge Intervals (https://leetcode.com/problems/merge-intervals/description/)

---
+++ src/leecode/MergeIntervals.java
@@ -0,0 +1,79 @@
+package leecode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import leecode.common.Interval;
+
+/***
+ * https://leetcode.com/problems/merge-intervals/description/
+ * 
+ * @author weiwei
+ *
+ */
+public class MergeIntervals {
+
+    public List<Interval> merge(List<Interval> intervals) {
+        if (intervals.size() < 2) return intervals;
+
+        List<Interval> res = new ArrayList<Interval>();
+
+        intervals.sort((i1, i2) -> Integer.compare(i1.start, i2.start));
+
+        int start = intervals.get(0).start;
+        int end = intervals.get(0).end;
+
+        for (int i = 1; i < intervals.size(); i++) {
+            Interval cur = intervals.get(i);
+            // overlap
+            if (end >= cur.start) {
+                end = Math.max(end, cur.end);
+            } else {
+                res.add(new Interval(start, end));
+                start = cur.start;
+                end = cur.end;
+            }
+        }
+        res.add(new Interval(start, end));
+
+        return res;
+    }
+
+    // https://discuss.leetcode.com/topic/38628/beat-98-java-sort-start-end-respectively
+    public List<Interval> merge2(List<Interval> intervals) {
+        // sort start&end
+        int n = intervals.size();
+        int[] starts = new int[n];
+        int[] ends = new int[n];
+        for (int i = 0; i < n; i++) {
+            starts[i] = intervals.get(i).start;
+            ends[i] = intervals.get(i).end;
+        }
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+
+        // loop through
+        List<Interval> res = new ArrayList<Interval>();
+        for (int i = 0, j = 0; i < n; i++) { // j is start of interval.
+            if (i == n - 1 || starts[i + 1] > ends[i]) {
+                res.add(new Interval(starts[j], ends[i]));
+                j = i + 1;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+        MergeIntervals c = new MergeIntervals();
+        List<Interval> intervals = new ArrayList<Interval>();
+        intervals.add(new Interval(1, 3));
+        intervals.add(new Interval(2, 6));
+        intervals.add(new Interval(8, 10));
+        intervals.add(new Interval(15, 18));
+
+        System.out.println(c.merge(intervals));
+        System.out.println(c.merge2(intervals));
+    }
+}
