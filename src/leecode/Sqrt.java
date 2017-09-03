c48c72b7ee21425393bacdf33cca0fa1fbb25a70
Weiwei Shi <shiweiwei97@gmail.com>
2017-09-03 21:10:53 +0800

    minor change

--- src/leecode/Sqrt.java
+++ src/leecode/Sqrt.java
@@ -9,15 +9,21 @@ package leecode;
 public class Sqrt {
 
     public int mySqrt(int x) {
-        long r = x;
+        long r = ((long) x + 1) >> 1;
         while (r * r > x)
-            r = (r + x / r) / 2;
+            r = (r + x / r) >> 1;
         return (int) r;
     }
 
     public static void main(String[] args) {
         Sqrt c = new Sqrt();
-        System.out.println(c.mySqrt(25));
+
+        System.out.println(c.mySqrt(0));
+        System.out.println(c.mySqrt(1));
+        System.out.println(c.mySqrt(5));
+        System.out.println(c.mySqrt(24));
+        System.out.println(c.mySqrt(26));
+        System.out.println(c.mySqrt(Integer.MAX_VALUE));
     }
 
 }
