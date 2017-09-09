2d7c2e71d5f6d5b6bb998664172425464ae968d3
Weiwei Shi <shiweiwei97@gmail.com>
2017-09-09 22:08:16 +0800

    another stack based solution for Largest rectangle in histogram

--- src/leecode/LargestRectangleInHistogram.java
+++ src/leecode/LargestRectangleInHistogram.java
@@ -8,6 +8,29 @@ package leecode;
  */
 public class LargestRectangleInHistogram {
 
+    public int largestRectangleArea2(int[] heights) {
+        int len = heights.length, res = 0, top = -1;
+        int[] stack = new int[len + 1];
+
+        for (int i = 0; i <= len; i++) {
+            // put a sentinel 0 as last bar
+            int height = (i == len) ? 0 : heights[i];
+
+            // pop stack while stack top > heights[i]
+            while (top != -1 && height < heights[stack[top]]) {
+                // height and width
+                int hh = heights[stack[top--]];
+                int width = (top == -1) ? i : i - 1 - stack[top];
+                res = Math.max(res, hh * width);
+            }
+
+            // now stack top <= heights[i]
+            stack[++top] = i;
+        }
+
+        return res;
+    }
+
     public int largestRectangleArea(int[] heights) {
 
         int len = heights.length;
@@ -45,6 +68,7 @@ public class LargestRectangleInHistogram {
         LargestRectangleInHistogram c = new LargestRectangleInHistogram();
 
         System.out.println(c.largestRectangleArea(new int[] { 2, 1, 5, 6, 2, 3 }));
+        System.out.println(c.largestRectangleArea2(new int[] { 2, 1, 5, 6, 2, 3 }));
 
     }
 
