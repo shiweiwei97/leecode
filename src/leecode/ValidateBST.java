b2600e4da8785418b67a8d89450bc85d22652842
Weiwei Shi <shiweiwei97@gmail.com>
2017-09-10 19:08:46 +0800

    Validate BST (https://leetcode.com/problems/validate-binary-search-tree/description/)

---
+++ src/leecode/ValidateBST.java
@@ -0,0 +1,21 @@
+package leecode;
+
+import leecode.common.TreeNode;
+
+/***
+ * https://leetcode.com/problems/validate-binary-search-tree/description/
+ * 
+ * @author weiwei
+ *
+ */
+public class ValidateBST {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean isValidBST(TreeNode root, long min, long max) {
+        if (root == null) return true;
+        if (root.val <= min || root.val >= max) return false;
+        return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+    }
+}
