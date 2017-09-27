e5d3c6eba00e2fca1ea81bc5d2a5c19ed49b65d5
Weiwei Shi <shiweiwei97@gmail.com>
2017-09-27 09:20:14 +0800

    DFS solution

--- src/leecode/SurroundedRegions.java
+++ src/leecode/SurroundedRegions.java
@@ -83,6 +83,45 @@ public class SurroundedRegions {
         return sb.toString();
     }
 
+    public void solve2(char[][] board) {
+
+        int m = board.length;
+        if (m == 0) return;
+
+        int n = board[0].length;
+        if (n == 0) return;
+
+        // at least 3x3 square can surround something
+        if (m < 3 || n < 3) return;
+
+        // start DFS from boundary
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') dfs(board, i, 0, m, n);
+            if (board[i][n - 1] == 'O') dfs(board, i, n - 1, m, n);
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (board[0][i] == 'O') dfs(board, 0, i, m, n);
+            if (board[m - 1][i] == 'O') dfs(board, m - 1, i, m, n);
+        }
+
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                board[i][j] = board[i][j] == 'B' ? 'O' : 'X';
+
+    }
+
+    private void dfs(char[][] board, int x, int y, int m, int n) {
+        if (x < 0 || x >= m || y < 0 || y >= n) return;
+        if (board[x][y] == 'O') board[x][y] = 'B';
+
+        // don't touch boundary cells, they will be covered anyways
+        if (x > 1 && board[x - 1][y] == 'O') dfs(board, x - 1, y, m, n);
+        if (y > 1 && board[x][y - 1] == 'O') dfs(board, x, y - 1, m, n);
+        if (x < m - 2 && board[x + 1][y] == 'O') dfs(board, x + 1, y, m, n);
+        if (y < n - 2 && board[x][y + 1] == 'O') dfs(board, x, y + 1, m, n);
+    }
+
     public static void main(String[] args) {
 
         SurroundedRegions c = new SurroundedRegions();
@@ -131,7 +170,7 @@ public class SurroundedRegions {
                 "XOOOOXOOOXXOOXOXOXOO".toCharArray()
 
         };
-        c.solve(board);
+        c.solve2(board);
         System.out.println(Arrays.deepToString(board));
 
         board = new char[][] {
