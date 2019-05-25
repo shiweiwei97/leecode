5fbfffb75ebe9984708cfdb93ac82eaebb4654b2
Weiwei Shi <shiweiwei97@gmail.com>
2019-05-25 21:51:59 +0800

    https://leetcode.com/problems/ipo/discuss/255679/Java-Short-Solution-!!!

--- src/leecode/IPO.java
+++ src/leecode/IPO.java
@@ -2,6 +2,7 @@ package leecode;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /***
  * https://leetcode.com/problems/ipo/
@@ -26,6 +27,35 @@ public class IPO {
         public String toString() {
             return "profit=" + profit + ", capital=" + capital + ", used=" + used + System.lineSeparator();
         }
+    }
+
+    /***
+     * https://leetcode.com/problems/ipo/discuss/255679/Java-Short-Solution-!!!
+     * 
+     * @param k
+     * @param W
+     * @param Profits
+     * @param Capital
+     * @return
+     */
+    public int findMaximizedCapital2(int k, int W, int[] Profits, int[] Capital) {
+
+        PriorityQueue<int[]> pqCap = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
+        for (int i = 0; i < Profits.length; i++)
+            pqCap.offer(new int[] { Capital[i], Profits[i] });
+
+        PriorityQueue<Integer> pqPro = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+
+        for (int i = 0; i < k; i++) {
+            while (!pqCap.isEmpty() && pqCap.peek()[0] <= W) {
+                pqPro.offer(pqCap.poll()[1]);
+            }
+
+            if (pqPro.isEmpty()) break;
+            W += pqPro.poll();
+        }
+
+        return W;
     }
 
     public int findMaximizedCapital(int k, int W, int[] Profits, int[] Capital) {
@@ -61,5 +91,7 @@ public class IPO {
         IPO c = new IPO();
 
         System.out.println(c.findMaximizedCapital(2, 0, new int[] { 1, 2, 3 }, new int[] { 0, 1, 1 }));
+
+        System.out.println(c.findMaximizedCapital2(2, 0, new int[] { 1, 2, 3 }, new int[] { 0, 1, 1 }));
     }
 }
